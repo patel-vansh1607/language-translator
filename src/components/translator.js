76f80498117839825ecc7043eeec6c073aa5430d
Vansh Patel <patel.vansh1607@gmail.com>
2025-03-22 14:51:12 +0300

    added initial Translator component with state management

---
+++ src/components/translator.js
@@ -0,0 +1,9 @@
+export default function Translator() {
+    const[inputFormat, setInputFormat] = useState('en');
+    const[outputFormat, setOutputFormat] = useState('hi');
+    const[translatedText, setTranslatedText] = useState('Translate');
+    cosnt[inputText, setInputText] = useState('');
+
+
+
+}

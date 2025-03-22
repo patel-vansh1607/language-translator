b502086acca377bb93990b67a408cf25b103dc1e
Vansh Patel <patel.vansh1607@gmail.com>
2025-03-22 14:56:37 +0300

    added translation functionality with API integration in Translator component

--- src/components/translator.js
+++ src/components/translator.js
@@ -23,5 +23,33 @@ export default function Translator() {
             document.querySelector('.translate').style.display = 'none';
     }
 
+    const url = 
+`https://microsoft-translator-text.p.rapidapi.com/translate?to%5B0%5D=${outputFormat}&api-version=3.0&profanityAction=NoAction&textType=plain`;
+        const options = {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+                'X-RapidAPI-Key': {"YOUR_API_KEY"},
+                'X-RapidAPI-Host': 'microsoft-translator-text.p.rapidapi.com'
+            },
+            body: JSON.stringify([
+                {
+                    Text: inputText
+                }
+            ])
+        };
+        try {
+            const response = await fetch(url, options);
+            const result = await response.text();
+            const responseObject = JSON.parse(result);
+            const translation = responseObject[0].translations[0].text;
+            setTranslatedText(translation);
+        } catch (error) {
+            console.log(error);
+            alert("Please Try Again! Some Error Occurred at your side");
+        }
+        document.querySelector('.fa.fa-spinner.fa-spin').style.display = "none";
+        document.querySelector('.translate').style.display = 'block';
+    }
 
 }

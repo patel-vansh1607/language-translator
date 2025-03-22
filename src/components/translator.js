54b50734adac69a257e7aee32d4ddf8d5c99da62
Vansh Patel <patel.vansh1607@gmail.com>
2025-03-22 14:53:13 +0300

    added language reversal functionality in Translator component

--- src/components/translator.js
+++ src/components/translator.js
@@ -2,7 +2,15 @@ export default function Translator() {
     const[inputFormat, setInputFormat] = useState('en');
     const[outputFormat, setOutputFormat] = useState('hi');
     const[translatedText, setTranslatedText] = useState('Translate');
-    cosnt[inputText, setInputText] = useState('');
+    const[inputText, setInputText] = useState('');
+
+    const handleReverseLanguage = () => {
+        const value = inputFormat;
+        setInputFormat = (outputFormat);
+        setOutputFormat(value);
+        setInputText('');
+        setTranslatedText('Translation');
+    }
 
 
 
